3ac6b0fb8a92c48d26c669e681a1e0848abf6885
DevelopEnvo <fightrajneesh08968@gmail.com>
2024-11-25 13:46:28 +0530

    mongodb url changes

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ const User = require('./models/User');
 const seedDB = require('./seed')
 const MongoStore = require('connect-mongo');
 
-const dbURL = process.env.dbURL || 'mongodb+srv://rajnishkumarrai8968:raj123@cluster0.nmfmm.mongodb.net';
-// || 'mongodb://localhost:27017/Shopping-rajnish-app'
+const dbURL = process.env.dbURL || 'mongodb://localhost:27017/Shopping-rajnish-app';
 mongoose.set('strictQuery', true);
 mongoose.connect(dbURL)
     .then(() => console.log('DB Connected'))
